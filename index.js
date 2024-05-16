2ece93595299b4e310add84c4ff26fcb6ffdbca6
runbarumba <166688257+runbarumba@users.noreply.github.com>
2024-05-16 07:01:19 +0000

    リクエストやまちゃん

--- index.js
+++ index.js
@@ -2,10 +2,36 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!松江高専佐々木爆笑!今日は前転です')
-})
 
+// GETリクエスト
+app.get('/', (req, res) => {
+  res.send({
+    msg:'GET request'
+  });
+});
+
+// POSTリクエスト
+app.post('/', (req, res) => {
+  res.send({
+    msg:'POST request'
+  });
+});
+
+// PUTリクエスト
+app.put('/:id', (req, res) => {
+  res.send({
+    id: req.params.id,
+    msg:'PUT request'
+  });
+});
+
+// DELETEリクエスト
+app.delete('/:id', (req, res) => {
+  res.send({
+    id: req.params.id,
+    msg:'DELETE request'
+  });
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
